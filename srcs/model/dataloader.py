47eb3a9092741643504cce55bd8c7c8ca75d27e7
Diego Agudelo <diego.agudeloc@hotmail.com>
2026-03-11 11:26:34 +0100

    Data Loader

--- srcs/model/dataloader.py
+++ srcs/model/dataloader.py
@@ -22,7 +22,42 @@ import pandas as pd
 from torch.utils.data import DataLoader as DL, TensorDataset
 import lightning as L
 
+
 class DataLoaderClass(L.LightningDataModule):
+
+    features = [
+        "MainBranch",
+        "Age",
+        "EdLevel",
+        "Employment",
+        "WorkExp",
+        "LearnCode",
+        "LearnCodeAI",
+        "YearsCode",
+        "DevType",
+        "OrgSize",
+        "ICorPM",
+        "RemoteWork",
+        "Industry",
+        "Country",
+        "Currency",
+        "CompTotal",
+        "LanguageChoice",
+        "LanguageHaveWorkedWith",
+        "DatabaseChoice",
+        "DatabaseHaveWorkedWith",
+        "PlatformChoice",
+        "PlatformHaveWorkedWith",
+        "WebframeChoice",
+        "WebframeHaveWorkedWith",
+        "DevEnvsChoice",
+        "DevEnvsHaveWorkedWith",
+        "AIModelsChoice",
+        "AISelect",
+        "AIAgents",
+    ]
+
+
     def __init__(self, path):
         super().__init__()
         try :
@@ -30,9 +65,39 @@ class DataLoaderClass(L.LightningDataModule):
         except Exception as e :
             print(f"Error : {e}")
             raise RuntimeError(f"Error : {e}")
+        
+        self.feature_engineering()
      
     def delete_features(self, features:list[str]) :
         self.df = self.df.drop(features)
 
     def feature_engineering(self):
-        self.df = 
+        self.df = self.df.loc[:,self.features]
+
+        self.df = self.df.dropna(subset="CompTotal")
+
+        # self.df["Currency"] = 
+
+
+    def __str__(self):
+        resume = f"{self.df}"
+        columns = f"{self.df.columns}"
+
+
+        return resume + "\n" + columns
+
+
+def main():
+    datapreprocess = DataLoaderClass("./datasets/survey_results_public.csv")
+
+
+    print(datapreprocess)
+
+    print(len(datapreprocess.df["Country"].unique()))
+    print(datapreprocess.df["Currency"].unique())
+    print(datapreprocess.df["CompTotal"])
+
+
+
+if __name__ == "__main__":
+    main()
